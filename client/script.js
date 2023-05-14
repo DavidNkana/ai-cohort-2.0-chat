49e15c8e06b6f07b81a9e84213763d7b4b7f6961
David Nkana <davidnkana74@gmail.com>
2023-05-14 09:16:16 -0700

    server link

--- client/script.js
+++ client/script.js
@@ -88,8 +88,7 @@ const handleSubmit = async (e) => {
     chatContainer.scrollTo(0, chatContainer.scrollHeight)
 
     try {
-        // https://chat-7icn.onrender.com
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://chat-7icn.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
